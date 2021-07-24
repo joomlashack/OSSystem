7d75783a424bcf3cbb8f40922dd36d386182e2c3
Bill Tomczak <bill@joomlashack.com>
2021-07-23 17:17:38 -0700

    Update installer script

--- src/script.installer.php
+++ src/script.installer.php
@@ -21,73 +21,70 @@
  * along with OSSystem.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Alledia\Installer;
+use Alledia\Installer\AbstractScript;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Installer\InstallerAdapter;
 
 defined('_JEXEC') or die();
 
+if (class_exists('\\PlgSystemOSSystemInstallerScript')) {
+    \Joomla\CMS\Factory::getApplication()->enqueueMessage(__FILE__, 'warning');
+    return;
+}
+
 require_once 'library/Installer/include.php';
 
-jimport('joomla.filesystem.folder');
 
-
-/**
- * Custom installer script
- */
-if (!class_exists('PlgSystemOSSystemInstallerScript')) {
-    class PlgSystemOSSystemInstallerScript extends Installer\AbstractScript
+class PlgSystemOSSystemInstallerScript extends AbstractScript
+{
+    /**
+     * @inheritDoc
+     */
+    public function preFlight(string $type, InstallerAdapter $parent): bool
     {
-        /**
-         * @param string                     $type
-         * @param JInstallerAdapterComponent $parent
-         *
-         * @return bool
+        if (!parent::preFlight($type, $parent)) {
+            return false;
+        }
+
+        /* Uninstall the deprecated plugin OSCARootCertificates.
+         * The parent method can't be used because the old plugin
+         * has a bug that doesn't allow to use the native uninstall method.
          */
-        public function preFlight($type, $parent)
-        {
-            if (!parent::preFlight($type, $parent)) {
-                return false;
-            }
+        $success = false;
 
-            /* Uninstall the depracated plugin OSCARootCertificates.
-             * The parent method can't be used because the old plugin
-             * has a bug that doesn't allow to use the native uninstall method.
-             */
-            $success = false;
+        // Remove the files
+        $path = JPATH_SITE . '/plugins/system/oscarootcertificates';
+        if (is_dir($path)) {
+            $success = Folder::delete($path);
+        }
 
-            // Remove the files
-            $path = JPATH_SITE . '/plugins/system/oscarootcertificates';
-            if (JFolder::exists($path)) {
-                $success = JFolder::delete($path);
-            }
+        // Remove the database row
+        $db = $this->dbo;
 
-            // Remove the database row
-            $db = JFactory::getDbo();
+        $queryWhere = [
+            $db->qn('type') . ' = ' . $db->q('plugin'),
+            $db->qn('element') . ' = ' . $db->q('oscarootcertificates'),
+            $db->qn('folder') . ' = ' . $db->q('system'),
+        ];
+        $query      = $db->getQuery(true)
+            ->select('COUNT(*)')
+            ->from('#__extensions')
+            ->where($queryWhere);
+        $db->setQuery($query);
 
-            $queryWhere = array(
-                $db->qn('type') . ' = ' . $db->q('plugin'),
-                $db->qn('element') . ' = ' . $db->q('oscarootcertificates'),
-                $db->qn('folder') . ' = ' . $db->q('system'),
-            );
+        if ((int)$db->loadResult() > 0) {
             $query = $db->getQuery(true)
-                ->select('COUNT(*)')
-                ->from('#__extensions')
+                ->delete('#__extensions')
                 ->where($queryWhere);
             $db->setQuery($query);
-
-            if ((int) $db->loadResult() > 0) {
-                $query = $db->getQuery(true)
-                    ->delete('#__extensions')
-                    ->where($queryWhere);
-                $db->setQuery($query);
-                $success = $db->execute();
-            }
-
-            // Displays the success message
-            if ((bool) $success) {
-                $this->setMessage('OSSystem OSCARootCertificates uninstalled successfully');
-            }
-
-            return true;
+            $success = $db->execute();
         }
+
+        // Displays the success message
+        if ($success) {
+            $this->sendMessage('OSSystem OSCARootCertificates uninstalled successfully');
+        }
+
+        return true;
     }
 }
